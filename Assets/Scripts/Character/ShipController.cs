f4a99d0af3dba4a916d1cfeafc5bfe9bbf346a9d
Алексей Скворцов <sotoder@gmail.com>
2022-09-12 20:45:47 +0300

    - выполнены дз 1 и 2

--- Assets/Scripts/Character/ShipController.cs
+++ Assets/Scripts/Character/ShipController.cs
@@ -4,7 +4,6 @@ using Network;
 using UI;
 using UnityEngine;
 using UnityEngine.Networking;
-
 namespace Characters
 {
     public class ShipController : NetworkMovableObject
@@ -12,19 +11,19 @@ namespace Characters
         public string PlayerName
         {
             get => playerName;
-            set => playerName = value;
+            set
+            {
+                playerName = value;
+                gameObject.name = value;
+            }
         }
-
         protected override float speed => shipSpeed;
-
         [SerializeField] private Transform cameraAttach;
         private CameraOrbit cameraOrbit;
         private PlayerLabel playerLabel;
         private float shipSpeed;
         private Rigidbody rb;
-
         [SyncVar] private string playerName;
-
         private void OnGUI()
         {
             if (cameraOrbit == null)
@@ -33,7 +32,6 @@ namespace Characters
             }
             cameraOrbit.ShowPlayerLabels(playerLabel);
         }
-
         public override void OnStartAuthority()
         {
             rb = GetComponent<Rigidbody>();
@@ -41,13 +39,13 @@ namespace Characters
             {
                 return;
             }
+            serverPosition = transform.position;
             gameObject.name = playerName;
             cameraOrbit = FindObjectOfType<CameraOrbit>();
             cameraOrbit.Initiate(cameraAttach == null ? transform : cameraAttach);
             playerLabel = GetComponentInChildren<PlayerLabel>();
             base.OnStartAuthority();
         }
-
         protected override void HasAuthorityMovement()
         {
             var spaceShipSettings = SettingsContainer.Instance?.SpaceShipSettings;
@@ -55,11 +53,11 @@ namespace Characters
             {
                 return;
             }
-
             var isFaster = Input.GetKey(KeyCode.LeftShift);
             var speed = spaceShipSettings.ShipSpeed;
             var faster = isFaster ? spaceShipSettings.Faster : 1.0f;
 
+            serverPosition = transform.position;
             shipSpeed = Mathf.Lerp(shipSpeed, speed * faster,
                 SettingsContainer.Instance.SpaceShipSettings.Acceleration);
 
@@ -67,10 +65,8 @@ namespace Characters
                 ? SettingsContainer.Instance.SpaceShipSettings.FasterFov
                 : SettingsContainer.Instance.SpaceShipSettings.NormalFov;
             cameraOrbit.SetFov(currentFov, SettingsContainer.Instance.SpaceShipSettings.ChangeFovSpeed);
-
             var velocity = cameraOrbit.transform.TransformDirection(Vector3.forward) * shipSpeed;
             rb.velocity = velocity * Time.deltaTime;
-
             if (!Input.GetKey(KeyCode.C))
             {
                 var targetRotation = Quaternion.LookRotation(
@@ -80,13 +76,35 @@ namespace Characters
             }
         }
 
-        protected override void FromServerUpdate() { }
+        protected override void FromServerUpdate()
+        {
+            transform.position = serverPosition;
+        }
         protected override void SendToServer() { }
 
         [ClientCallback]
         private void LateUpdate()
         {
             cameraOrbit?.CameraMovement();
+            gameObject.name = playerName;
+        }
+
+        [ServerCallback]
+        public void OnTriggerEnter(Collider other)
+        {
+            RpcChangePosition(
+                new Vector3(
+                    Random.Range(50,150),
+                    Random.Range(50, 150),
+                    Random.Range(50, 150)));
+        }
+
+        [ClientRpc]
+        public void RpcChangePosition(Vector3 position)
+        {
+            gameObject.SetActive(false);
+            transform.position = position;
+            gameObject.SetActive(true);
         }
     }
 }
--- Assets/Scripts/Main/SolarSystemNetworkManager.cs
+++ Assets/Scripts/Main/SolarSystemNetworkManager.cs
@@ -1,20 +1,106 @@
 using Characters;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Main
 {
     public class SolarSystemNetworkManager : NetworkManager
     {
-        [SerializeField] private string playerName;
+        [SerializeField] private InputField _playerNameField;
+        [SerializeField] private Button _startServerButton;
+        [SerializeField] private Button _stopServerButton;
+        [SerializeField] private Button _startClientButton;
+        [SerializeField] private Button _stopClientButton;
+
+        private Dictionary<int, ShipController> _shipMatchings = new Dictionary<int, ShipController>();
+        private NetworkManager _manager;
+
+        private void Awake()
+        {
+            _manager = GetComponent<NetworkManager>();
+
+            _startServerButton.onClick.AddListener(ManualStartServer);
+            _stopServerButton.onClick.AddListener(_manager.StopServer);
+            _startClientButton.onClick.AddListener(ManualStartClient);
+            _stopClientButton.onClick.AddListener(_manager.StopClient);
+        }
+
+        private void ManualStartServer()
+        {
+            _manager.StartServer();
+        }
+
+        private void ManualStartClient()
+        {
+            _manager.StartClient();
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            NetworkServer.RegisterHandler(100, ReciveLoginMessege);
+        }
 
         public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
         {
             var spawnTransform = GetStartPosition();
 
             var player = Instantiate(playerPrefab, spawnTransform.position, spawnTransform.rotation);
-            player.GetComponent<ShipController>().PlayerName = playerName;
+            var ship = player.GetComponent<ShipController>();
+            _shipMatchings.Add(conn.connectionId, ship);
+
             NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            base.OnClientConnect(conn);
+            var login = new LoginMessege();
+            login.messege = _playerNameField.text == "" ? "Player" + conn.connectionId.ToString() : _playerNameField.text;
+
+            conn.Send(100, login);
+        }
+
+        public void ReciveLoginMessege(NetworkMessage message)
+        {
+            var loginName = message.reader.ReadString();
+            _shipMatchings[message.conn.connectionId].PlayerName = loginName;
+        }
+
+        private void Update()
+        {
+            ChekInputKey();
+        }
+
+        private void ChekInputKey()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                ManualStartServer();
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                ManualStartClient();
+            }
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                _manager.StopServer();
+            }
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                _manager.StopClient();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _startServerButton.onClick.RemoveListener(ManualStartServer);
+            _stopServerButton.onClick.RemoveListener(StopServer);
+            _startClientButton.onClick.RemoveListener(ManualStartClient);
+            _stopClientButton.onClick.RemoveListener(StopClient);
         }
     }
 }
