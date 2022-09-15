062baa5b54550ac98ca13efe6c564589caba663e
Алексей Скворцов <sotoder@gmail.com>
2022-09-15 21:47:47 +0300

    - дочищено

--- Assets/Scripts/Mechanics/ObjectMover.cs
+++ Assets/Scripts/Mechanics/ObjectMover.cs
@@ -114,15 +114,6 @@ namespace Mechanics
 
                 var cristallRotationJobHandler = cristallRotationJob.Schedule(_cristallsTransform);
                 cristallRotationJobHandler.Complete();
-
-                //for(int i = 0; i < _cristallsTransform.length; i++)
-                //{
-                //    _manager.SendVector4(new Vector4(_cristallsTransform[i].rotation.x,
-                //                                              _cristallsTransform[i].rotation.y,
-                //                                              _cristallsTransform[i].rotation.z,
-                //                                              _cristallsTransform[i].rotation.w), 102);
-                //    _manager.SendInt(i, 103);
-                //}
             }
         }
 
